e1bdecbd902cccf0475a7f68bc8688ba0be90c6f
sajadsani <aliakbarisani.sajad@gmail.com>
2022-04-06 19:40:35 -0400

    adding stochastic gradient descent

--- Ch4/regression.py
+++ Ch4/regression.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import SGDRegressor
 
 num_samples = 100
 X = 2*np.random.rand(num_samples,1)
@@ -27,7 +28,7 @@ if 1==0: # implementing LR using normal equation
         print(lin_reg.intercept_, lin_reg.coef_)
         X_new = np.array([[0], [2]])
         print(lin_reg.predict(X_new))
-else: # implementing LR using gradient descent
+elif 1==0: # implementing LR using gradient descent
     eta = 0.1 # learning rate
     n_iterations = 1000
     theta = np.random.randn(2,1) # random initialization
@@ -35,3 +36,23 @@ else: # implementing LR using gradient descent
         gradients = 2/num_samples * X_b.T.dot(X_b.dot(theta) - y)
         theta = theta - eta * gradients
     print(theta)
+elif 1==1: # implementing LR using Stochastic gradient descent by hand 
+    if 1==0: # implementing SGD by hand
+        n_epochs = 50
+        t0, t1 = 5, 50 # learning schedule hyperparameters  
+        def learning_schedule(t):
+            return t0 / (t + t1)
+        theta = np.random.randn(2,1) # random initialization
+        for epoch in range(n_epochs):
+            for i in range(num_samples):
+                random_index = np.random.randint(num_samples)
+                xi = X_b[random_index:random_index+1]
+                yi = y[random_index:random_index+1]
+                gradients = 2 * xi.T.dot(xi.dot(theta) - yi)
+                eta = learning_schedule(epoch * num_samples + i)
+                theta = theta - eta * gradients 
+        print(theta) 
+    else:
+        sgd_reg = SGDRegressor(max_iter=1000, tol=1e-3, penalty=None, eta0=0.1)
+        sgd_reg.fit(X, y.ravel())    
+        print(sgd_reg.intercept_, sgd_reg.coef_)
